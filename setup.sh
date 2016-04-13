d24ea13bbfb99d707ed5d74e616fb8644bde9e40
chxuan <787280310@qq.com>
2016-04-13 16:04:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@ cp .ycm_extra_conf.py ~
 
 mkdir ~/.vim
 rm -rf ~/.vim/plugin
+rm -rf ~/.vim/colors
 cp -R ./plugin ~/.vim
+cp -R ./colors ~/.vim
 
 echo "正在克隆Vundle.vim！"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
