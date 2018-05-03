bf10af90450304c49545dcd5777a34d7651df3ae
chengxuan <chengxuan@klicen.com>
2018-05-03 22:04:53 +0800

    update

--- install.sh
+++ install.sh
@@ -178,6 +178,7 @@ function chown_dir()
     who_is=$(who)
     current_user=${who_is%% *}
     sudo chown -R ${current_user}:${current_user} ~/.vim
+    sudo chown -R ${current_user}:${current_user} ~/.fonts
     sudo chown -R ${current_user}:${current_user} ~/.cache
     sudo chown ${current_user}:${current_user} ~/.vimrc.local
     sudo chown ${current_user}:${current_user} ~/.viminfo
