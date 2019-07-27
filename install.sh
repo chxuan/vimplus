5efd65cb896233574c94a3fc3da76a66025286d0
程煊 <chengxuan@klicen.com>
2019-07-27 18:43:21 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,25 @@ function get_platform_type()
     echo $(uname)
 }
 
-# 获取linux平台类型
-function get_linux_platform_type()
+# 获取linux发行版名称
+function get_linux_distro()
 {
-    if which zypper > /dev/null ; then
-        echo "opensuse"
-    elif which apt-get > /dev/null ; then
-        echo "ubuntu" # debian,ubuntu,deepin,linuxmint
-    elif which yum > /dev/null ; then
-        echo "centos" # centos,redhat
-    elif which pacman > /dev/null; then
-        echo "archlinux"
+    if grep -Eq "Ubuntu" /etc/*-release; then
+        echo "Ubuntu"
+    elif grep -Eq "Deepin" /etc/*-release; then
+        echo "Deepin"
+    elif grep -Eq "LinuxMint" /etc/*-release; then
+        echo "LinuxMint"
+    elif grep -Eq "Debian" /etc/*-release; then
+        echo "Debian"
+    elif grep -Eq "CentOS" /etc/*-release; then
+        echo "CentOS"
+    elif grep -Eq "openSUSE" /etc/*-release; then
+        echo "openSUSE"
+    elif grep -Eq "ArchLinux" /etc/*-release; then
+        echo "ArchLinux"
     else
-        echo "invaild"
+        echo "Unknow"
     fi
 }
 
@@ -48,6 +54,40 @@ function compile_vim_on_ubuntu()
     sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
         libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
         libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 lua5.1-dev
+
+    curl https://ftp.nluug.nl/pub/vim/unix/vim-8.1.tar.bz2 -o ~/vim.tar.bz2
+    tar -xvf ~/vim.tar.bz2 -C ~
+    cd ~/vim81
+    ./configure --with-features=huge \
+        --enable-multibyte \
+        --enable-rubyinterp \
+        --enable-pythoninterp \
+        --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+        --enable-perlinterp \
+        --enable-luainterp \
+        --enable-gui=gtk2 \
+        --enable-cscope \
+        --prefix=/usr
+    make
+    sudo make install
+    cd -
+}
+
+# 在debian上源代码安装vim
+function compile_vim_on_debian()
+{
+    sudo apt-get remove -y vim vim-runtime gvim
+    sudo apt-get remove -y vim-tiny vim-common vim-gui-common vim-nox
+    sudo rm -rf /usr/bin/vim*
+    sudo rm -rf /usr/local/bin/vim*
+    sudo rm -rf /usr/share/vim/vim*
+    sudo rm -rf /usr/local/share/vim/vim*
+    rm -rf ~/vim.tar.bz2
+    rm -rf ~/vim81
+
+    sudo apt-get install -y libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev ruby-dev lua5.1 lua5.1-dev
+
+    sudo ln -s /lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/libtinfo.so.5
 
     curl https://ftp.nluug.nl/pub/vim/unix/vim-8.1.tar.bz2 -o ~/vim.tar.bz2
     tar -xvf ~/vim.tar.bz2 -C ~
@@ -110,14 +150,7 @@ function install_prepare_software_on_mac()
     brew install vim gcc cmake ctags-exuberant curl ack
 }
 
-# 安装centos发行版必要软件
-function install_prepare_software_on_centos()
-{
-    sudo yum install -y ctags automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel curl fontconfig ack bzip2 git
-    compile_vim_on_centos
-}
-
-# 安装ubuntu发行版必要软件
+# 安装ubuntu必要软件
 function install_prepare_software_on_ubuntu()
 {
     sudo apt-get update
@@ -133,13 +166,28 @@ function install_prepare_software_on_ubuntu()
     compile_vim_on_ubuntu
 }
 
-# 安装archlinux发行版必要软件
+# 安装debian必要软件
+function install_prepare_software_on_debian()
+{
+    sudo apt-get update
+    sudo apt-get install -y cmake ctags build-essential python python-dev python3-dev fontconfig curl libfile-next-perl ack-grep git
+    compile_vim_on_debian
+}
+
+# 安装centos必要软件
+function install_prepare_software_on_centos()
+{
+    sudo yum install -y ctags automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel curl fontconfig ack bzip2 git
+    compile_vim_on_centos
+}
+
+# 安装archlinux必要软件
 function install_prepare_software_on_archlinux()
 {
     sudo pacman -S --noconfirm vim ctags automake gcc cmake python3 python2 curl ack git
 }
 
-# 安装opensuse发行版必要软件
+# 安装opensuse必要软件
 function install_prepare_software_on_opensuse()
 {
     sudo zypper install -y vim ctags gcc gcc-c++ cmake python-devel python3-devel curl ack fontconfig git ncurses5-devel
@@ -274,50 +322,63 @@ function begin_install_vimplus()
     print_logo
 }
 
-# 在ubuntu发行版安装vimplus
+# 在ubuntu上安装vimplus
 function install_vimplus_on_ubuntu()
 {
     install_prepare_software_on_ubuntu
     begin_install_vimplus
 }
 
-# 在centos发行版安装vimplus
+# 在debian上安装vimplus
+function install_vimplus_on_debian()
+{
+    install_prepare_software_on_debian
+    begin_install_vimplus
+}
+
+# 在centos上安装vimplus
 function install_vimplus_on_centos()
 {
     install_prepare_software_on_centos
     begin_install_vimplus
 }
 
-# 在archlinux发行版安装vimplus
+# 在archlinux上安装vimplus
 function install_vimplus_on_archlinux()
 {
     install_prepare_software_on_archlinux
     begin_install_vimplus
 }
 
-# 在opensuse发行版安装vimplus
+# 在opensuse上安装vimplus
 function install_vimplus_on_opensuse()
 {
     install_prepare_software_on_opensuse
     begin_install_vimplus
 }
 
-# 在linux平台安装vimplus
+# 在linux平上台安装vimplus
 function install_vimplus_on_linux()
 {
-    type=`get_linux_platform_type`
-    echo "Linux platform type: "${type}
+    distro=`get_linux_distro`
+    echo "Linux distro: "${distro}
 
-    if [ ${type} == "ubuntu" ]; then
+    if [ ${distro} == "Ubuntu" ]; then
         install_vimplus_on_ubuntu
-    elif [ ${type} == "centos" ]; then
+    elif [ ${distro} == "Deepin" ]; then
+        install_vimplus_on_ubuntu
+    elif [ ${distro} == "LinuxMint" ]; then
+        install_vimplus_on_ubuntu
+    elif [ ${distro} == "Debian" ]; then
+        install_vimplus_on_debian
+    elif [ ${distro} == "CentOS" ]; then
         install_vimplus_on_centos
-    elif [ ${type} == "archlinux" ]; then
-        install_vimplus_on_archlinux
-    elif [ ${type} == "opensuse" ]; then
+    elif [ ${distro} == "openSUSE" ]; then
         install_vimplus_on_opensuse
+    elif [ ${distro} == "ArchLinux" ]; then
+        install_vimplus_on_archlinux
     else
-        echo "Not support this linux platform type: "${type}
+        echo "Not support this linux platform distro: "${distro}
     fi
 }
 
